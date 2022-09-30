68f2744daa2d830cc10e8df413d081132b5bc451
stk346 <stk346@gamil.com>
2022-10-01 00:06:34 +0900

    회원가입하기 완료.

--- blog/src/main/resources/static/js/user.js
+++ blog/src/main/resources/static/js/user.js
@@ -1,6 +1,6 @@
 let index = {
     init: function() {
-        $("#btn-save").on("click", ()=> {
+        $("#btn-save").on("click", ()=> { // function(){} 대신 화살표 함수를 사용한 이유는 this를 바인딩하기 위해서
             this.save();
         });
     },
@@ -15,7 +15,22 @@ let index = {
 
 //        console.log(data);
 
-          $.ajax().done.fail(); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
+          // 회원가입 수행 요청
+          // ajax 호출시 default가 비동기 호출
+          // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
+          $.ajax({
+            type: "POST",
+            url: "/blog/api/user",
+            data: JSON.stringify(data), // http body 데이터
+            contentType: "application/json; charset=utf-8", // body데이터가 어떤 타입인지(MIME)
+            dataType: "json" // 요청을 서버로 해서 응답이 왔을 때 기본적으로 모든 것이 문자열. / 생긴게 json이라면 -> javascript 오브젝트로 변경
+          }).done(function(resp){ // 성공하면
+            alert("회원가입이 완료돠었습니다");
+//            console.log(resp);
+            location.href = "/blog";
+          }).fail(function(error){ // 실패하면
+            alert(JSON.stringify(error));
+          });
     }
 }
 
