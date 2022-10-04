9b7955b06033f736e0669333497602219353452d
stk346 <stk346@gamil.com>
2022-10-04 13:36:35 +0900

    임시커밋

--- blog/src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ blog/src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -18,10 +18,10 @@ public class UserApiController {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private HttpSession session; // session 객체는 spring container가 bin으로 등록해서 가지고 있기 때문에 @AutoWired로 가능하다.
+//    @Autowired
+//    private HttpSession session; // session 객체는 spring container가 bin으로 등록해서 가지고 있기 때문에 @AutoWired로 가능하다.
 
-    @PostMapping("/api/user")
+    @PostMapping("/auth/joinProc")
     public ResponseDto<Integer> save(@RequestBody User user) { // username, password, email
         System.out.println("UserApiController : save 호출됨");
         // 실제로 DB에 insert를 하고 아래에서 return이 되면 된다.
@@ -31,15 +31,15 @@ public class UserApiController {
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // Jackson이 자바 오브젝트를 JSON으로 변환해서 리턴
     }
 
-    // 다음에는 스프링 시큐리티를 이용해서 로그인 해볼 것이다.
-    @PostMapping("/api/user/login")
-    public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
-        System.out.println("UserApiController : login 호출됨");
-        User principal = userService.로그인(user); // principal: 접근 주체라는 뜻
-
-        if (principal != null) {
-            session.setAttribute("principal", principal);
-        }
-        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // Jackson이 자바 오브젝트를 JSON으로 변환해서 리턴
-    }
+    // 스프링의 전통적인 로그인 방식(시큐리티 사용x)
+//    @PostMapping("/api/user/login")
+//    public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
+//        System.out.println("UserApiController : login 호출됨");
+//        User principal = userService.로그인(user); // principal: 접근 주체라는 뜻
+//
+//        if (principal != null) {
+//            session.setAttribute("principal", principal);
+//        }
+//        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // Jackson이 자바 오브젝트를 JSON으로 변환해서 리턴
+//    }
 }
--- blog/src/main/java/com/cos/blog/service/UserService.java
+++ blog/src/main/java/com/cos/blog/service/UserService.java
@@ -16,12 +16,12 @@ public class UserService {
 
     // Transactional로 인해 회원가입 전체 서비스가 하나의 transaction으로 묶임. 다 끝나면 커밋됨
     @Transactional
-    public  void 회원가입(User user) {
+    public void 회원가입(User user) {
         userRepository.save(user);
     }
 
-    @Transactional(readOnly = true) // Select할 때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료 (정합성을 유지시킬 수 있음.)
-    public User 로그인(User user) {
-        return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
-    }
+//    @Transactional(readOnly = true) // Select할 때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료 (정합성을 유지시킬 수 있음.)
+//    public User 로그인(User user) {
+//        return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
+//    }
 }
